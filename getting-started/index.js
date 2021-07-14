a47dc216e2ccad3900a02b56c1ee9e87c60b7149
heejin lee <heejinlee07@gmail.com>
2021-07-14 10:19:58 +0900

    Add: 설정파일 적용

---
+++ getting-started/index.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
+// ts -> js로 변환하는 컴파일을 위해 설치
+// npm i typescript -g
+// 설치 확인 tsc -v
---
+++ getting-started/index.ts
@@ -0,0 +1,10 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(10, 20);
+
+// ts -> js로 변환하는 컴파일을 위해 설치
+// npm i typescript -g
+// 설치 확인 tsc -v
+// ts -> js로 컴파일 tsc index.ts
