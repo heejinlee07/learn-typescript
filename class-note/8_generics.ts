c2f7012ef1f75e21df603b89721a62fdd29e6b66
heejin lee <heejinlee07@gmail.com>
2021-07-16 17:08:26 +0900

    Add: generic

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -58,3 +58,51 @@ interface GenericDropdown<T> {
 
 //<string>이면 value 값의 타입은 string이 되고, <number>이면 value 값의 타입은 number이다.
 const obj2: GenericDropdown<string> = { value: 'a', selected: false };
+
+//* 제네릭의 타입 제한 - T는 반환하지 않고 생략해도 되지만 명시적으로 선언한 것.
+//1. logTextLength<T>(text:T):T 라고 사용하면 logTextLength('hi');를 넘겼더라도 console.log(text.length);에서 에러가 난다.
+//TS 입장에서는 이 시점에 length가 있는지 없는지 알지 못하기 때문.
+//2. 타입을 배열로 작성하면 length를 구할 수 있다.
+function logTextLength<T>(text: T[]): T[] {
+  console.log(text.length);
+  text.forEach(function (text) {
+    console.log(text);
+  });
+  return text;
+}
+
+logTextLength(['hi', 'abc']);
+
+//* 제네릭의 타입 제한 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+
+//제네릭으로 받은 타입은 항상 LengthType의 하위 타입이므로 LengthType 에 있는 속성이 있는 상태에서 추가되는 것.
+function logTextLength2<T extends LengthType>(text: T): T {
+  text.length;
+  return text;
+}
+
+logTextLength2('a'); //o
+logTextLength2(10); //x length가 제공되지 않기 때문.
+logTextLength2({ length: 10 }); //o
+
+//* 제네릭의 타입 제한 - keyof
+interface ShoppingItems {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+//ShoppingItems에 있는 key 중 한 가지가 제네릭이 된다
+function getShoppingItemOption<T extends keyof ShoppingItems>(
+  itemOption: T
+): T {
+  return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption('a');
+
+getShoppingItemOption('name');
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,29 +1,37 @@
-interface Email {
-  value: string;
+interface DropdownItem<T> {
+  value: T;
   selected: boolean;
 }
 
+// //! DropdownItem을 제네릭으로 쓰면, 아래 정의된 Email, ProductNumber, TrunFalse interface는 없어도 된다.
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+// interface TrunFalse {
+//   value: boolean;
+//   selected: boolean;
+// }
+
 //1. { value: string; selected: boolean }[]
-//2. interface Email
-const emails: Email[] = [
+//2. interface Email -> Email[]
+//3. generic - DropdownItem<string>[]
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
-
-interface TrunFalse {
-  value: boolean;
-  selected: boolean;
-}
-
 // 1.{ value: number; selected: boolean }[]
-// 2. interface ProdeuctNumber
-const numberOfProducts: ProductNumber[] = [
+// 2. interface ProdeuctNumber - ProductNumber[]
+// 3. generic - DropdownItem<number>[]
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
@@ -32,10 +40,16 @@ const numberOfProducts: ProductNumber[] = [
 //1. item: { value: string; selected: boolean }
 //2. 유니언 타입 정의 - item: { value: number; selected: boolean } | { value: string; selected: boolean }
 //3. 인터페이스를 이용한 유니언 타입 정의 item: Email | ProductNumber
-
+//4. generic의 유니언 타입 정의 item: DropdownItem<string> | DropdownItem<number>
+//5. 함수 파라미터에 유니언이 들어가는 코드를 제네릭을 넘겨서 사용하면 더 깔끔.
+/*
+함수 호출시 타입을 같이 넘기고, 
+createDropdownItem<string>(email)
+function createDropdownItem<T>(item: DropdownItem<T>) 이런식으로 사용하여 유니언을 제거할 수 있다.
+*/
 //emails, numerOfProducts의 타입을 모두 수용할 수 있는 타입 정의가 필요.
 //제네릭 활용
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
